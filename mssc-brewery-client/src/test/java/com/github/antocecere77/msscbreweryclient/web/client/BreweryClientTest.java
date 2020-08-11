6bdbd59b78152df13f8de0c0ff987f95ef2e860f
antonio <a.cecere.esterno@almaviva.it>
2020-08-11 09:46:34 +0200

    Http delete with spring resttemplate

--- mssc-brewery-client/src/test/java/com/github/antocecere77/msscbreweryclient/web/client/BreweryClientTest.java
+++ mssc-brewery-client/src/test/java/com/github/antocecere77/msscbreweryclient/web/client/BreweryClientTest.java
@@ -41,5 +41,8 @@ class BreweryClientTest {
         client.updateBeer(UUID.randomUUID(), beerDto);
     }
 
-
+    @Test
+    void testDeleteBeer() {
+        client.deleteBeer(UUID.randomUUID());
+    }
 }
