49e677a5e684b2b6d49d66b59c43c358e6e7629a
antonio <a.cecere.esterno@almaviva.it>
2020-08-11 16:54:08 +0200

    Example of using project lombok and mapstruct

---
+++ mssc-brewery-master/src/main/java/com/antocecere77/msscbrewery/web/mappers/BeerMapper.java
@@ -0,0 +1,13 @@
+package com.antocecere77.msscbrewery.web.mappers;
+
+import com.antocecere77.msscbrewery.domain.Beer;
+import com.antocecere77.msscbrewery.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+
+    BeerDto beerToBeerDto(Beer beer);
+
+    Beer beerDtoToBeer(BeerDto beerDto);
+}
