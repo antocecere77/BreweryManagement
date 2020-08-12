5d311f66cc04c34da876422d97d3a7d74207ed5f
antonio <a.cecere.esterno@almaviva.it>
2020-08-12 16:40:35 +0200

    Monolith project code review

---
+++ brewery-monolith/target/generated-sources/annotations/guru/sfg/brewery/web/mappers/BeerOrderLineMapperImpl.java
@@ -0,0 +1,15 @@
+package guru.sfg.brewery.web.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-08-12T16:36:39+0200",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.6 (JetBrains s.r.o)"
+)
+@Component
+@Primary
+public class BeerOrderLineMapperImpl extends BeerOrderLineMapperDecorator implements BeerOrderLineMapper {
+}
