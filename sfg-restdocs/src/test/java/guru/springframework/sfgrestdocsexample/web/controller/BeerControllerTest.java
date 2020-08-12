4e5722e161d6316d2b0682581b5a4141ac506707
antonio <a.cecere.esterno@almaviva.it>
2020-08-12 11:01:35 +0200

    Documentation generation

--- sfg-restdocs/src/test/java/guru/springframework/sfgrestdocsexample/web/controller/BeerControllerTest.java
+++ sfg-restdocs/src/test/java/guru/springframework/sfgrestdocsexample/web/controller/BeerControllerTest.java
@@ -59,7 +59,7 @@ class BeerControllerTest {
                 .param("iscold", "yes")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(document("v1/beer",
+                .andDo(document("v1/beer-get",
                         pathParameters (
                                 parameterWithName("beerId").description("UUID of desired beer to get.")
                         ),
@@ -90,7 +90,7 @@ class BeerControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
                 .andExpect(status().isCreated())
-                .andDo(document("v1/beer",
+                .andDo(document("v1/beer-new",
                         requestFields(
                                 fields.withPath("id").ignored(),
                                 fields.withPath("version").ignored(),
