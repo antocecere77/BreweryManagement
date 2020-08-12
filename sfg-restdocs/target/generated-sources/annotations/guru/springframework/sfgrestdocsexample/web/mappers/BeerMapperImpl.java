a319e2195c655ee49c52c63fdbc6cb64da2075a2
antonio <a.cecere.esterno@almaviva.it>
2020-08-12 10:28:37 +0200

    Documenting validation constraints

--- sfg-restdocs/target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
+++ sfg-restdocs/target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-08-12T09:02:50+0200",
+    date = "2020-08-12T10:22:56+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 14.0.1 (Oracle Corporation)"
 )
 @Component
