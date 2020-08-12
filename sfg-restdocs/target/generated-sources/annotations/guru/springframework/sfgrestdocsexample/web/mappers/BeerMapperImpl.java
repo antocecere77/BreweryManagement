cb06a2664f832b7e11e57fcc3c651087dad8e1d0
antonio <a.cecere.esterno@almaviva.it>
2020-08-12 11:13:55 +0200

    Serving docs with spring boot

--- sfg-restdocs/target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
+++ sfg-restdocs/target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-08-12T10:57:18+0200",
+    date = "2020-08-12T11:09:10+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 14.0.1 (Oracle Corporation)"
 )
 @Component
