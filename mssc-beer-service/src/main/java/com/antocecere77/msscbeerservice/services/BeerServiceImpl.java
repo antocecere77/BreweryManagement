e131c03e68febea2216ed9078f6ac954f8807bdc
antocecere <antocecere77@gmail.com>
2020-08-21 07:59:26 +0200

    Add caching to get beer api

--- mssc-beer-service/src/main/java/com/antocecere77/msscbeerservice/services/BeerServiceImpl.java
+++ mssc-beer-service/src/main/java/com/antocecere77/msscbeerservice/services/BeerServiceImpl.java
@@ -8,6 +8,8 @@ import com.antocecere77.msscbeerservice.web.model.BeerDto;
 import com.antocecere77.msscbeerservice.web.model.BeerPagedList;
 import com.antocecere77.msscbeerservice.web.model.BeerStyleEnum;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,7 @@ import org.springframework.util.StringUtils;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class BeerServiceImpl implements BeerService {
@@ -23,11 +26,14 @@ public class BeerServiceImpl implements BeerService {
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
 
+    @Cacheable(cacheNames = "beerListCache", condition = "#showInventoryOnHand == false ")
     @Override
     public BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest, Boolean showInventoryOnHand) {
 
         BeerPagedList beerPagedList;
         Page<Beer> beerPage;
+
+        log.info("listBeers was called");
 
         if (!StringUtils.isEmpty(beerName) && !StringUtils.isEmpty(beerStyle)) {
             //search both
@@ -66,8 +72,11 @@ public class BeerServiceImpl implements BeerService {
         return beerPagedList;
     }
 
+    @Cacheable(cacheNames = "beerCache", key = "#beerId", condition = "#showInventoryOnHand == false ")
     @Override
     public BeerDto getById(UUID beerId, Boolean showInventoryOnHand) {
+
+        log.info("getById was called");
         if(showInventoryOnHand) {
             return beerMapper.beerToBeerDtoWithInventory(beerRepository.findById(beerId)
                     .orElseThrow(NotFoundException::new));
