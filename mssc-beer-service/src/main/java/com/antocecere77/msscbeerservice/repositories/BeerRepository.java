f30b3313f2c95742d28329c3f0ff1d41fea6943c
antocecere <antocecere77@gmail.com>
2020-09-05 11:29:53 +0200

    Create brewing service

--- mssc-beer-service/src/main/java/com/antocecere77/msscbeerservice/repositories/BeerRepository.java
+++ mssc-beer-service/src/main/java/com/antocecere77/msscbeerservice/repositories/BeerRepository.java
@@ -1,14 +1,14 @@
 package com.antocecere77.msscbeerservice.repositories;
 
-import com.antocecere77.msscbeerservice.domain.*;
+import com.antocecere77.msscbeerservice.domain.Beer;
 import com.antocecere77.msscbeerservice.web.model.BeerStyleEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
 
     Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
 
