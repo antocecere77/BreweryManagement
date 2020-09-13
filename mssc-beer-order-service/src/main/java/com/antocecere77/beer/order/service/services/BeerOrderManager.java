4bc3a3c65a34f29a54bb7df0ffdf16d2edf58274
antocecere <antocecere77@gmail.com>
2020-09-13 12:32:20 +0200

    Beer order manager service

---
+++ mssc-beer-order-service/src/main/java/com/antocecere77/beer/order/service/services/BeerOrderManager.java
@@ -0,0 +1,8 @@
+package com.antocecere77.beer.order.service.services;
+
+import com.antocecere77.beer.order.service.domain.BeerOrder;
+
+public interface BeerOrderManager {
+
+    BeerOrder newBeerOrder(BeerOrder beerOrder);
+}
