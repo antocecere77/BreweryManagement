255f8dd3baa4546cd60159d1af92bda099e6966a
antocecere <antocecere77@gmail.com>
2020-09-20 15:59:05 +0200

    Handle validate order response

--- mssc-beer-order-service/src/main/java/com/antocecere77/beer/order/service/services/BeerOrderManager.java
+++ mssc-beer-order-service/src/main/java/com/antocecere77/beer/order/service/services/BeerOrderManager.java
@@ -2,7 +2,11 @@ package com.antocecere77.beer.order.service.services;
 
 import com.antocecere77.beer.order.service.domain.BeerOrder;
 
+import java.util.UUID;
+
 public interface BeerOrderManager {
 
     BeerOrder newBeerOrder(BeerOrder beerOrder);
+
+    void processValidationResult(UUID beerOrderId, Boolean isValid);
 }
