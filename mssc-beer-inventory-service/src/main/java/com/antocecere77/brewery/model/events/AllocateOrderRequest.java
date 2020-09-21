6a0b117a07e47a6534c7c36d09d62d17b9f819f8
antocecere <antocecere77@gmail.com>
2020-09-21 08:09:43 +0200

    Complete allocation message flow (inventory)

---
+++ mssc-beer-inventory-service/src/main/java/com/antocecere77/brewery/model/events/AllocateOrderRequest.java
@@ -0,0 +1,15 @@
+package com.antocecere77.brewery.model.events;
+
+import com.antocecere77.brewery.model.BeerOrderDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AllocateOrderRequest {
+    private BeerOrderDto beerOrderDto;
+}
--- mssc-beer-order-service/src/main/java/com/antocecere77/beer/order/service/services/BeerOrderManager.java
+++ mssc-beer-order-service/src/main/java/com/antocecere77/beer/order/service/services/BeerOrderManager.java
@@ -1,6 +1,7 @@
 package com.antocecere77.beer.order.service.services;
 
 import com.antocecere77.beer.order.service.domain.BeerOrder;
+import com.antocecere77.brewery.model.BeerOrderDto;
 
 import java.util.UUID;
 
@@ -9,4 +10,10 @@ public interface BeerOrderManager {
     BeerOrder newBeerOrder(BeerOrder beerOrder);
 
     void processValidationResult(UUID beerOrderId, Boolean isValid);
+
+    void beerOrderAllocationPassed(BeerOrderDto beerOrder);
+
+    void beerOrderAllocationPendingInventory(BeerOrderDto beerOrder);
+
+    void beerOrderAllocationFailed(BeerOrderDto beerOrder);
 }
