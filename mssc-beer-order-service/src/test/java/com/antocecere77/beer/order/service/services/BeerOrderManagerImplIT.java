885adf6a09eaca360bc8bed3ac8d0d53250f80e8
antocecere <antocecere77@gmail.com>
2020-09-25 08:04:24 +0200

    Failed allocation compensating transaction

--- mssc-beer-order-service/src/test/java/com/antocecere77/beer/order/service/services/BeerOrderManagerImplIT.java
+++ mssc-beer-order-service/src/test/java/com/antocecere77/beer/order/service/services/BeerOrderManagerImplIT.java
@@ -1,5 +1,6 @@
 package com.antocecere77.beer.order.service.services;
 
+import com.antocecere77.beer.order.service.config.JmsConfig;
 import com.antocecere77.beer.order.service.domain.BeerOrder;
 import com.antocecere77.beer.order.service.domain.BeerOrderLine;
 import com.antocecere77.beer.order.service.domain.BeerOrderStatusEnum;
@@ -9,6 +10,7 @@ import com.antocecere77.beer.order.service.repositories.CustomerRepository;
 import com.antocecere77.beer.order.service.services.beer.BeerServiceImpl;
 import com.antocecere77.brewery.model.BeerDto;
 import com.antocecere77.brewery.model.BeerPagedList;
+import com.antocecere77.brewery.model.events.AllocationFailureEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.jenspiegsa.wiremockextension.WireMockExtension;
@@ -20,6 +22,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jms.core.JmsTemplate;
 
 import java.util.*;
 
@@ -30,6 +33,7 @@ import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMoc
 import static org.awaitility.Awaitility.await;
 import static org.jgroups.util.Util.assertEquals;
 import static org.jgroups.util.Util.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(WireMockExtension.class)
 @SpringBootTest
@@ -49,6 +53,9 @@ public class BeerOrderManagerImplIT {
 
     @Autowired
     WireMockServer wireMockServer;
+
+    @Autowired
+    JmsTemplate jmsTemplate;
 
     Customer testCustomer;
 
@@ -167,6 +174,11 @@ public class BeerOrderManagerImplIT {
             BeerOrder foundOrder = beerOrderRepository.findById(beerOrder.getId()).get();
             assertEquals(BeerOrderStatusEnum.ALLOCATION_EXCEPTION, foundOrder.getOrderStatus());
         });
+
+        AllocationFailureEvent allocationFailureEvent = (AllocationFailureEvent) jmsTemplate.receiveAndConvert(JmsConfig.ALLOCATE_FAILURE_QUEUE);
+
+        assertNotNull(allocationFailureEvent);
+        assertThat(allocationFailureEvent.getOrderId()).isEqualTo(savedBeerOrder.getId());
     }
 
     @Test
