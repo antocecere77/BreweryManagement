c7901993347edcef1f43eebf8a15d28c7b61c9aa
antocecere <antocecere77@gmail.com>
2020-10-03 11:22:11 +0200

    Inventory failover - initial project add

---
+++ mssc-inventory-failover/src/test/java/com/antocecere77/brewery/inventoryfailover/SfgBreweryInventoryFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antocecere77.brewery.inventoryfailover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgBreweryInventoryFailoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
